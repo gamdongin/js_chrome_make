b53701bc6136e9d2dd20c4d2ad99f97b9301c917
sifjhsh <sifjhsh@gmail.com>
2025-11-12 21:25:14 +0900

    2.4

--- jsjs.js
+++ jsjs.js
@@ -1,20 +1,12 @@
-let a = 12;
-let b = 3;
-let c = 0;
+const a = true;
+let b;
+const c = null;
 
 // let 재선언 금지, 재할당 가능
 // const 재선언 금지, 재할당 금지
 // var 재선언 가능, 재할당 가능
 // var 쓰면 대가리 깨짐
 
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
-
-c = a;
-a = b;
-b = c;
-
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
+console.log(a, b, c);
+console.log(b);
+console.log(c);
