33ae5387912501f4ac8c566a8050864b384fe09c
sifjhsh <sifjhsh@gmail.com>
2025-11-12 21:07:07 +0900

    2.3

--- jsjs.js
+++ jsjs.js
@@ -1,6 +1,19 @@
-const a = 12;
-const b = 3;
-// _ 를 포함한 특수 기호 X
+let a = 12;
+let b = 3;
+let c = 0;
+
+// let 재선언 금지, 재할당 가능
+// const 재선언 금지, 재할당 금지
+// var 재선언 가능, 재할당 가능
+// var 쓰면 대가리 깨짐
+
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+
+c = a;
+a = b;
+b = c;
 
 console.log(a + b);
 console.log(a / b);
