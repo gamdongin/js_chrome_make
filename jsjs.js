3c2542aa81866339d23e4985685b4f82e7f0e970
sifjhsh <sifjhsh@gmail.com>
2025-11-13 15:30:17 +0900

    3.5

--- jsjs.js
+++ jsjs.js
@@ -1,14 +1,20 @@
 function a(){
-    console.log("click");
     b.style.color = "red";
 }
 function c(){
     b.style.color = "blue";
 }
-
+function d(){
+    b.style.color = "green";
+    console.log("click");
+    b.removeEventListener("mouseenter", a);
+    b.removeEventListener("mouseleave", c);
+    // .addEventListener 는 .removeEventListener 로 삭제가능
+}
 const b = document.querySelector("div.uuuu h1");
-console.log(b);
-
+b.addEventListener("click", d);
 b.addEventListener("mouseenter", a);
+// b.onmouseenter = a;
 b.addEventListener("mouseleave", c);
-
+// b.onmouseleave = c;
+window.addEventListener("copy", function(){this.alert("copyyyyyyyyy");});
