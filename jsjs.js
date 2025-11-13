9f06971a2494fbe25f461f0732ceb1fd19a70c66
sifjhsh <sifjhsh@gmail.com>
2025-11-13 12:16:11 +0900

    3.1

--- jsjs.js
+++ jsjs.js
@@ -1,5 +1,4 @@
-console.dir(document)
-// 개체의 속성을 보여줌
-console.log(document)
-// 객체의 요소를 보여줌
-document.title = "ㅈㄱㄴ"
+const a = document.getElementById("title");
+a.innerText = "i got you"
+console.log(a.id);
+console.log(a.className);
