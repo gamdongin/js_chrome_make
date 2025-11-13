4ee7ba73427deb601f7821488645d0c358249176
sifjhsh <sifjhsh@gmail.com>
2025-11-13 11:47:20 +0900

    2.16

--- jsjs.js
+++ jsjs.js
@@ -9,7 +9,27 @@ if (condition){
 else if(age<=18 || age>60){
     console.log("nope");
 }
-// || : or , && : and
+// || : or , && : and , === : 등호
 else{
     console.log("적셔!");
 }
+
+// == 은 값만을 비교하고 === 은 유형도 비교
+// 0 == false ---> true
+// 0 === false ---> false
+
+alert(1 == '1') //true
+alert(1 === '1') //false
+alert(null == undefined) //true
+alert(null === undefined) //false
+alert(true == 1); //true
+alert(true === 1) //false
+alert(true == '1') //true
+alert(true === '1') //false
+
+// 배열, 객체는 같은 같을 가져도 다른 주소를 참조함
+
+var a = [1,2,3];
+var b = [1,2,3];
+console.log(a == b); // false
+console.log(a === b); // false
