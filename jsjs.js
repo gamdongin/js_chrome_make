d9aca61c6b2dcb9d7d0590b1d91a0ab7be800f2e
sifjhsh <sifjhsh@gmail.com>
2025-11-13 11:36:27 +0900

    2.15

--- jsjs.js
+++ jsjs.js
@@ -1,9 +1,15 @@
-// prompt() 함수는 사용자에게 창을 띄어 값을 받음.
-// prompt()를 사용하면 답을 할때까지 코드의 실행을 멈추고, 매우 오래된 방법임. css로 바꾸지도 못함.
-// 틀딱을 넘어선 해골딱딱 함수
-
-const a = prompt("값 입력");
-// 기본적으로 문자열 타입
-console.log(typeof a, typeof parseInt(a));
-console.log(a, parseInt(a));
+age = parseInt(prompt("나이"));
+console.log(isNaN(age));
 // NaN : not a number
+
+condition = isNaN(age)
+if (condition){
+    console.log("이거 숫자 아닌데?");
+}
+else if(age<=18 || age>60){
+    console.log("nope");
+}
+// || : or , && : and
+else{
+    console.log("적셔!");
+}
