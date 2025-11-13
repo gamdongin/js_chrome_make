32379ce825c49b2856d41120e6443ae4aeb69281
sifjhsh <sifjhsh@gmail.com>
2025-11-13 09:58:18 +0900

    2.6

--- jsjs.js
+++ jsjs.js
@@ -1,8 +1,13 @@
-const b = "wqwdwd";
-const a = [b,2,3,2,12313,43523243,"1234145325",436235,1232];
+const a = {
+    a : "123sad",
+    b : 123123,
+    c : null,
+    d : true
+};
 
 console.log(a);
-console.log(a[4]);
-
-a.push(99999); // 맨 뒤에 추가, 맵핑한 주소가 아닌 데이터 자체가 바뀌기에 const 도 가능
-console.log(a[-1]);
+a.qwe = "잇";
+console.log(a);
+console.log(a.d);
+a.d = false;
+console.log(a.d);
