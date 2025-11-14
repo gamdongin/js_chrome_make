ccb77f34910548fb8c4fd473603e9ca4444dbe64
sifjhsh <sifjhsh@gmail.com>
2025-11-14 11:48:07 +0900

    3.8

--- jsjs.js
+++ jsjs.js
@@ -1,14 +1,17 @@
 const b = document.querySelector(".uuuu h1");
-console.log(b);
+const first = b.className; // 최초의 클래스
+
 function a(){
-    colorB = b.style.color
-    if (colorB === "red"){
-        b.style.color = "blue";
+    // classB = b.className; 작동 안함
+
+    const asd = "asd";
+    if (b.className === asd+" "+first){
+        b.className = first;
+        // 기존의 클래스는 삭제하고 새로 지정
+    } else {
+        b.className = asd+" "+first;
+        //  html에 클래스를 지정하고 그걸 css가 감지
     }
-    else{
-        b.style.color = "red";
-    }
-    console.log(b.style.color);
 }
 
 b.addEventListener("click", a);
