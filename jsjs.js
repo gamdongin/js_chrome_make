d3630ea1e817a5e4a500f7bab5edda7d208be348
sifjhsh <sifjhsh@gmail.com>
2025-11-14 11:53:02 +0900

    3.8_1

--- jsjs.js
+++ jsjs.js
@@ -2,15 +2,12 @@ const b = document.querySelector(".uuuu h1");
 const first = b.className; // 최초의 클래스
 
 function a(){
-    // classB = b.className; 작동 안함
-
+    // toggle (메소드로 만든 함수)는 신이다.
     const asd = "asd";
-    if (b.className === asd+" "+first){
-        b.className = first;
-        // 기존의 클래스는 삭제하고 새로 지정
+    if (b.classList.contains(asd)){
+        b.classList.remove(asd);
     } else {
-        b.className = asd+" "+first;
-        //  html에 클래스를 지정하고 그걸 css가 감지
+        b.classList.add(asd);
     }
 }
 
