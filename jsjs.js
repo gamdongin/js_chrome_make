c64c58330071eccd0eab16414bd1267a5eafad5c
sifjhsh <sifjhsh@gmail.com>
2025-11-14 19:52:30 +0900

    4.4

--- jsjs.js
+++ jsjs.js
@@ -2,14 +2,18 @@ const formLogin = document.querySelector("#form-login");
 const inputLogin = formLogin.querySelector("input");
 const buttonLogin = formLogin.querySelector("button");
 
-function consolView(event){event.preventDefault();console.log(event);}
-formLogin.addEventListener("submit", consolView);
+const hiddenH1 = document.querySelector("#greeting");
+const HIDDEN_VISI = "hiddenVisi";
+const HIDDEN_DISP = "hiddenDisp";
+// 문자열만 저장하는 변수는 대문자와 _ 로 작성하는 관습이있다.
 
-const link = document.querySelector("a");
-
-function clickHandle(event){
-
-    alert("누르면 이동")
+function consolView(event){
+    event.preventDefault();
+    const userName = inputLogin.value;
+    formLogin.classList.add(HIDDEN_VISI);
+    hiddenH1.innerText = `Hi ${userName}`;
+    // ``는 파이썬의 f"" 와 비슷함  // f" {var}" // ` ${var}`
+    hiddenH1.classList.remove(HIDDEN_DISP);
 }
 
-link.addEventListener("click", consolView);
+formLogin.addEventListener("submit", consolView);
