5a5426aa11dbdf68120285300f176a3fe9d3a4a1
sifjhsh <sifjhsh@gmail.com>
2025-11-14 16:22:30 +0900

    4.1

--- jsjs.js
+++ jsjs.js
@@ -7,8 +7,13 @@ const buttonLogin = formLogin.querySelector("button");
 // const buttonLogin = document.querySelector("#form-login button");
 
 function consolView(){
-    if (inputLogin.value === ""){
-        console.log("이름 작성");
+    const name = inputLogin.value
+    if (name === ""){
+        alert("이름 작성");
+    } else if (name.length > 15){
+        // html 자체적으로 제한 가능 , 그냥 연습용
+        //  근데 form 쓰면 자동으로 url 이 바뀌고 새로고침돼서 이런것도 필요
+        alert("15자 이내로 ");
     } else {
         console.log("hello "+inputLogin.value);
     }
