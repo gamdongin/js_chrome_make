6245da094ede2572664671435f035fd780841f26
sifjhsh <sifjhsh@gmail.com>
2025-11-14 19:30:33 +0900

    4.3

--- jsjs.js
+++ jsjs.js
@@ -1,16 +1,15 @@
 const formLogin = document.querySelector("#form-login");
-
 const inputLogin = formLogin.querySelector("input");
 const buttonLogin = formLogin.querySelector("button");
 
-// const inputLogin = document.querySelector("#form-login input");
-// const buttonLogin = document.querySelector("#form-login button");
+function consolView(event){event.preventDefault();console.log(event);}
+formLogin.addEventListener("submit", consolView);
 
-function consolView(event){
-    // 무슨 이름이든 첫번째 공간에 매개변수가 오면 방금 일어난 이벤트를 전달함
-    event.preventDefault();
-    // 기본 행동을 막음 여기선 새로고침
-    console.log(event);
+const link = document.querySelector("a");
+
+function clickHandle(event){
+
+    alert("누르면 이동")
 }
 
-formLogin.addEventListener("submit", consolView);
+link.addEventListener("click", consolView);
