6d657e99a347c3ab033c773387a66ea9eb20f468
sifjhsh <sifjhsh@gmail.com>
2025-11-14 11:58:18 +0900

    3.8_2

--- jsjs.js
+++ jsjs.js
@@ -2,13 +2,7 @@ const b = document.querySelector(".uuuu h1");
 const first = b.className; // 최초의 클래스
 
 function a(){
-    // toggle (메소드로 만든 함수)는 신이다.
-    const asd = "asd";
-    if (b.classList.contains(asd)){
-        b.classList.remove(asd);
-    } else {
-        b.classList.add(asd);
-    }
+    // toggle (메소드 중 하나)는 신이다.
+    b.classList.toggle("asd");
 }
-
 b.addEventListener("click", a);
