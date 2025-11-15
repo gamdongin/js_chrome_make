46372f86d6549d9f98e5100b6f3971cbc7b95520
sifjhsh <sifjhsh@gmail.com>
2025-11-15 13:34:11 +0900

    5.2

--- js/clock.js
+++ js/clock.js
@@ -7,10 +7,11 @@ let viewTime = `${hour} : ${minute} : ${second}`;
 
 function sayHello(){
     let date = new Date();
-    second = date.getSeconds();
-    minute = date.getMinutes();
-    hour = date.getHours();
+    second = String(date.getSeconds()).padStart(2,"0");
+    minute = String(date.getMinutes()).padStart(2,"0");
+    hour = String(date.getHours()).padStart(2,"0");
     viewTime = `${hour} : ${minute} : ${second}`;
     clock.innerText = viewTime;
+    // .padStart( lin,"A") : lin 만큼의 길이 중 빈곳을 앞쪽 부터 A 로 채움
 }
 setInterval(sayHello, 1000);
