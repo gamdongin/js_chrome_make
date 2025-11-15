24462875f1f679887cee65c3793d4feb14e386e3
sifjhsh <sifjhsh@gmail.com>
2025-11-15 13:13:17 +0900

    5.1-1

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,6 @@
 const clock = document.querySelector("h2#clock");
-let second = 0;
-let minute = 0;
-let hour = 0;
-let viewTime = `${hour} : ${minute} : ${second}`;
-
 function sayHello(){
-    second += 1;
-    if (second === 60){
-        minute += 1;
-        second = 0;
-        if (minute === 60){
-            hour += 1;
-            minute = 0;
-        }
-    }
-    viewTime = `${hour} : ${minute} : ${second}`;
-    clock.innerText = viewTime;
+    clock.innerText = "time out";
 }
-
-setInterval(sayHello, 1000);
-// setInterval(해당 함수를, t ms 마다 실행)
+setTimeout(sayHello, 2000);
+// setInterval(해당 함수를, t ms 후 한번 실행)
