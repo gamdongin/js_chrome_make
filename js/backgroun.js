54e7dce3d0ed196bb6da79e9d50396e20758c236
sifjhsh <sifjhsh@gmail.com>
2025-11-16 21:52:30 +0900

    6.1참고

--- js/backgroun.js
+++ js/backgroun.js
@@ -1,8 +1,8 @@
 const imgArray = ["6057300.jpg","6060118.jpg","jepum-baechi-3d-geulim-e-daehan-chusang-baegyeong-ip-geulimja-bich-seutyudio-bin-baegyeong-jepum-diseupeullei-jangmyeon.jpg"];
 
 const img = imgArray[Math.floor(Math.random()*imgArray.length)];
-const bgImg = document.createElement("img");
 
-bgImg.src = `img/background/${img}`;
-
-document.body.appendChild(bgImg);
+const bgImg = document.createElement("style");
+bgImg.innerText = `body {background-image: url(img/background/${img});
+    ground-position: center; background-repeat: no-repeat; background-size: cover; }`;
+document.head.appendChild(bgImg);
